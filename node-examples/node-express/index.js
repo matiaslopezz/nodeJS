a81ad4ecc5b42d6562da7cfcae788a55f737c825
Matías López <69003973+matiaslopezz@users.noreply.github.com>
2022-01-15 18:25:05 -0300

    First Assignment

--- node-examples/node-express/index.js
+++ node-examples/node-express/index.js
@@ -6,6 +6,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -15,6 +17,8 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use((req, res, next) => {
   console.log(req.headers);

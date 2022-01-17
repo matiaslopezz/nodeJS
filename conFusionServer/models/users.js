c3652d64fc1dc128d70bfb67f10d670364b4368f
Matías López <69003973+matiaslopezz@users.noreply.github.com>
2022-01-17 16:35:24 -0300

    Passport

--- conFusionServer/models/users.js
+++ conFusionServer/models/users.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
     username: {
@@ -16,5 +17,15 @@ var User = new Schema({
         default: false
     }
 });
+
+var User = new Schema({
+    admin:   {
+        type: Boolean,
+        default: false
+    }
+});
+
+User.plugin(passportLocalMongoose);
+
 
 module.exports = mongoose.model('User', User);

8a254e19e0b2e858f4141f1a18f9a9b6203fc184
Matías López <69003973+matiaslopezz@users.noreply.github.com>
2022-01-18 18:59:33 -0300

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: 'Your Client App ID',
-        clientSecret: 'Your Client App Secret'
+        clientId: '942270763081756',
+        clientSecret: 'db96766ce6820c18c8aad892225a4aa2'
     }
 }
